ffc37db563b5208277c97d9343c64fd7e836d546
stefanimajic <stefani.majic@gmail.com>
2018-07-23 18:55:37 +0200

    Added Sikuli C# examples

---
+++ SeleniumAndSikuli/SeleniumAndSikuli/GoogleSearch.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using Sikuli4Net.sikuli_UTIL;
+using Sikuli4Net.sikuli_REST;
+using OpenQA.Selenium.Chrome;
+using System.Threading;
+using SikuliSharp;
+
+namespace SeleniumAndSikuli
+{
+    [TestClass]
+    public class GoogleSearch
+    {
+        private IWebDriver webDriver = null;
+        private APILauncher launcher = new APILauncher(true);
+
+
+        [TestMethod]
+        public void GoogleSearchTest()
+        {
+            launcher.Start();
+            GetWebDriver("https://www.google.com/");
+
+            Screen screen = new Screen();
+            Pattern pattern_SearchText = new Pattern(@"c:\users\stefani\documents\visual studio 2017\Projects\SeleniumAndSikuli\SeleniumAndSikuli\Images\SearchText.PNG");
+            Pattern pattern_SearchButton = new Pattern(@"c:\users\stefani\documents\visual studio 2017\Projects\SeleniumAndSikuli\SeleniumAndSikuli\Images\SearchButton.PNG");
+            Pattern pattern_GoogleIcon = new Pattern(@"c:\users\stefani\documents\visual studio 2017\Projects\SeleniumAndSikuli\SeleniumAndSikuli\Images\Google.PNG");
+
+            screen.Wait(pattern_SearchText, 250);
+            screen.Type(pattern_SearchText, "Test", KeyModifier.NONE);
+            screen.Wait(pattern_GoogleIcon, 200);
+            screen.Click(pattern_GoogleIcon);
+
+            screen.Wait(pattern_SearchButton, 200);
+            screen.Click(pattern_SearchButton);
+
+            Thread.Sleep(200);
+            webDriver.Quit();
+
+            launcher.Stop();
+        }
+
+        //[TestMethod]
+        //public void ClickDesktopIcon()
+        //{
+        //    laucher.Start();
+        //    Screen screen = new Screen();
+        //    Pattern pattern_FolderIcon = new Pattern("path/FolderIcon.PNG")
+        //    screen.Wait(pattern_FolderIcon, 500);
+        //    screen.DoubleClick(pattern_FolderIcon);
+        //    launcher.Stop();
+        //}
+
+        private void GetWebDriver(string url)
+        {
+            webDriver = new ChromeDriver();
+            webDriver.Manage().Window.Maximize();
+            webDriver.Navigate().GoToUrl(url);
+            Thread.Sleep(1500);
+        }
+
+    
+}
+}
---
+++ SeleniumAndSikuli/SeleniumAndSikuli/WinAuth.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using Sikuli4Net.sikuli_UTIL;
+using Sikuli4Net.sikuli_REST;
+using OpenQA.Selenium.Chrome;
+using System.Threading;
+using OpenQA.Selenium.Firefox;
+
+namespace SeleniumAndSikuli
+{
+    [TestClass]
+    public class WinAuth
+    {
+        private IWebDriver webDriver = null;
+        private APILauncher launcher = new APILauncher(true);
+
+        [TestMethod]
+        public void WinAuthLogin()
+        {
+            launcher.Start();
+            GetWebDriver("https://myappwithwinauth");
+
+            Screen screen = new Screen();
+
+            //Chrome patterns
+            //Pattern pattern_username = new Pattern(@"c:\users\stefani\documents\visual studio 2017\Projects\SeleniumAndSikuli\SeleniumAndSikuli\Images\username.PNG");
+            //Pattern pattern_password = new Pattern(@"c:\users\stefani\documents\visual studio 2017\Projects\SeleniumAndSikuli\SeleniumAndSikuli\Images\password.PNG");
+            //Pattern pattern_button = new Pattern(@"c:\users\stefani\documents\visual studio 2017\Projects\SeleniumAndSikuli\SeleniumAndSikuli\Images\button.PNG");
+
+            //Mozilla Firefox patterns
+            Pattern pattern_username = new Pattern(@"c:\users\stefani\documents\visual studio 2017\Projects\SeleniumAndSikuli\SeleniumAndSikuli\Images\MFusername.PNG");
+            Pattern pattern_password = new Pattern(@"c:\users\stefani\documents\visual studio 2017\Projects\SeleniumAndSikuli\SeleniumAndSikuli\Images\MFpassword.PNG");
+            Pattern pattern_button = new Pattern(@"c:\users\stefani\documents\visual studio 2017\Projects\SeleniumAndSikuli\SeleniumAndSikuli\Images\MFbutton.PNG");
+
+
+            screen.Wait(pattern_username, 500);
+            screen.Type(pattern_username, "test", KeyModifier.NONE);
+
+            screen.Wait(pattern_password, 500);
+            screen.Type(pattern_password, "test", KeyModifier.NONE);
+
+            screen.Wait(pattern_button, 200);
+            screen.Click(pattern_button);
+
+
+
+            Thread.Sleep(200);
+           // webDriver.Quit();
+            launcher.Stop();
+        }
+
+        private void GetWebDriver(string url)
+        {
+            //webDriver = new ChromeDriver();
+            webDriver = new FirefoxDriver();
+            webDriver.Manage().Window.Maximize();
+            webDriver.Navigate().GoToUrl(url);
+            Thread.Sleep(2000);
+        }
+    }
+}
